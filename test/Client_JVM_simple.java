74cd0ee22758aec0cff20910e0edfb130b2266c8
William Duong <35279699+w-duong@users.noreply.github.com>
2020-07-17 18:07:32 -0700

    modified Client_JVM_simple to test file transfer

--- test/Client_JVM_simple.java
+++ test/Client_JVM_simple.java
@@ -16,10 +16,68 @@ public class Client_JVM_simple
     private static DataOutputStream goingOut;
     private static DataInputStream comingIn;
 
+    public void sendFile () throws Exception
+    {
+        try
+        {
+            File outboundFile = new File ("GraphingFundamentals.png");
+            FileInputStream fstream = new FileInputStream(outboundFile);
+            BufferedInputStream bfstream = new BufferedInputStream (fstream);
+
+            byte [] sendBuffer = new byte [4096];
+            int remaining = 0; 
+            int soFar = 0;
+
+            while ((remaining = bfstream.read(sendBuffer, 0, sendBuffer.length)) != -1)
+            {
+                goingOut.write(sendBuffer, 0, remaining);
+                soFar += remaining;
+                System.out.println ("Amount of data sent > " + soFar);
+            }
+
+            goingOut.flush();
+            bfstream.close();
+            fstream.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println (e.getMessage());
+        }
+    }
+
+    public void recvFile (String fileName) throws Exception
+    {
+        String path = "/Desktop/" + filename;
+
+        try
+        {
+            File incomingFile = new File (System.getProperty("user.home"), path);
+            OutputStream writeToFile = new FileOutputStream (incomingFile);
+
+            byte [] readBuffer = new byte [1024];
+            int readEachTime = 0;
+            int soFar = 0;
+
+            while ((readEachTime = comingIn.read(readBuffer, 0, readBuffer.length)) != -1)
+            {
+                writeToFile.write(readBuffer, 0, readEachTime);
+                soFar += readEachTime;
+
+                System.out.println ("Read from Server > " + soFar);
+            }
+
+            writeToFile.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println (e.getMessage());
+        }
+    }
+
     public static void main (String [] args) throws IOException
     {
         // RETRIEVE IP-ADDRESS BY (1) LOOKUP VS (2) MANUAL ASSIGNMENT
-        String ip = "192.168.0.25"; // (2)
+        String ip = "192.168.1.46"; // (2)
 
         // ESTABLISH CONNECTION USING IP AND PORT OF _SERVER_
         // NOTE: client also opens its own socket, but this is system assigned and
@@ -95,6 +153,16 @@ public class Client_JVM_simple
                             comingIn.close();
 
                             this.exit = true;
+                        }
+                        else if (messageIn.contains("ACKNSEND"))
+                            sendFile();
+                        else if (messageIn.contains("//FRIENDFILE"))
+                        {
+                            String [] packet = messageIn.split("@");
+                            String [] prefix = packet[0].split(">>:")
+                            String fileName = prefix[1];
+
+                            recvFile(fileName);
                         }
 
                         System.out.println (messageIn);
